3c36842c0406f792c36fb10c787b3bc8485abba9
Сергей <goroxov.99@mail.ru>
2026-03-09 21:45:08 +0300

    Add comments in module api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -10,25 +10,29 @@ from api.views.users import (UserChangePasswordApiView, UserListApiView,
                              UserRetrieveUpdateDestroyApiView,
                              UserRetrieveUpdateDestroyOtherUsersApiView)
 
+# Viewsets
 router = DefaultRouter()
 router.register(r'roles', RoleViewSet, basename='roles')
 router.register(r'elements', ElementViewSet, basename='elements')
 router.register(r'access_roles_rules', AccessRolesRulesViewSet, basename='access_roles_rules')
 
 urlpatterns = [
-    path("register/", UserRegisterApiView.as_view()),
-
+    # Эндпоинты работы с jwt токенами
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # Эндпоинты работы с пользователем
+    path("register/", UserRegisterApiView.as_view()),
     path('users/', UserListApiView.as_view()),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyOtherUsersApiView.as_view()),
     path('users/me/', UserRetrieveUpdateDestroyApiView.as_view()),
     path('users/me/change_password/', UserChangePasswordApiView.as_view()),
 
+    # Эндпоинты работы с продуктами
     path('products/', ProductsView.as_view()),
     path('products/<int:pk>/', ProductsView.as_view()),
 
+    # Эндпоинты работы с заказами
     path('orders/', OrdersView.as_view()),
     path('orders/<int:pk>/', OrdersView.as_view()),
 
