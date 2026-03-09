cfc89e7902dd624507e384d9bf75eeda0b1c079c
Сергей <goroxov.99@mail.ru>
2026-03-09 21:30:03 +0300

    Connection admin panel

--- elements/admin.py
+++ elements/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from elements.models import Element
+
+admin.site.register(Element)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User, Role, AccessRolesRules
+
+admin.site.register(User)
+admin.site.register(Role)
+
+
+@admin.register(AccessRolesRules)
+class AccessRolesRulesAdmin(admin.ModelAdmin):
+    list_display = ["role", "element", "read_all_permission", "create_permission", "update_all_permission",
+                    "delete_all_permission"]
+    list_filter = ["role", "element"]
