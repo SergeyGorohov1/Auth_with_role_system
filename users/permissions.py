95291216aa4e6e46ef48af850662ec67fc2c2a5f
Сергей <goroxov.99@mail.ru>
2026-03-10 13:58:45 +0300

    Add permissions for Django Views

---
+++ users/permissions.py
@@ -0,0 +1,33 @@
+from elements.models import Element
+from users.models import AccessRolesRules, User
+
+
+def can_access_object(request, object):
+    role = request.user.role
+
+    try:
+        element = Element.objects.get(name=object)
+    except Element.DoesNotExist:
+        return False
+
+    if role:
+        try:
+            access_roles_rules = AccessRolesRules.objects.get(role=role, element=element)
+            if request.method == "POST":
+                return access_roles_rules.create_permission
+            elif request.method == "GET":
+                return access_roles_rules.read_all_permission
+            elif request.method in ["PUT", "PATCH"]:
+                return access_roles_rules.update_all_permission
+            elif request.method == "DELETE":
+                return access_roles_rules.delete_all_permission
+        except AccessRolesRules.DoesNotExist:
+            return False
+    return False
+
+
+def is_owner(request, obj):
+    if type(obj) is User:
+        return obj == request.user
+    else:
+        return obj.owner == request.user
