bda026708b3151a1866039bbebe7cafa20be3de0
Сергей <goroxov.99@mail.ru>
2026-03-10 13:59:04 +0300

    Add CRUD for elements

--- elements/urls.py
+++ elements/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from elements.views import home
+from elements.views import home, ElementsListView, ElementsCreateView, ElementsUpdateView, ElementsDeleteView
 from elements.apps import ElementsConfig
 
 app_name = ElementsConfig.name
 
 urlpatterns = [
-    path('', home, name='home')]
+    path('', home, name='home'),
+
+    path('elements/', ElementsListView.as_view(), name='elements_list'),
+    path('elements/create/', ElementsCreateView.as_view(), name='elements_create'),
+    path('elements/<int:pk>/update/', ElementsUpdateView.as_view(), name='elements_update'),
+    path('elements/<int:pk>/delete/', ElementsDeleteView.as_view(), name='elements_delete'),
+
+    # path('products/', ElementsListView.as_view(), name='products_list'),
+    # path('products/create/', ElementsListView.as_view(), name='products_create'),
+    # path('products/<int:pk>/update/', ElementsListView.as_view(), name='products_update'),
+    # path('products/<int:pk>/delete/', ElementsListView.as_view(), name='products_delete'),
+    #
+    # path('orders/', ElementsListView.as_view(), name='orders_list'),
+    # path('orders/create/', ElementsListView.as_view(), name='orders_create'),
+    # path('orders/<int:pk>/update/', ElementsListView.as_view(), name='orders_update'),
+    # path('orders/<int:pk>/delete/', ElementsListView.as_view(), name='orders_delete'),
+]
--- elements/views.py
+++ elements/views.py
@@ -1,4 +1,56 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from elements.forms import ElementForm
+from users.permissions import can_access_object
+
+from elements.models import Element
+
 
 def home(request):
-    return render(request, "base.html")
+    elements = Element.objects.all()
+    return render(request, "home.html", {"elements": elements})
+
+
+class ElementsListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Element
+    template_name = "list.html"
+    extra_context = {"name": "Элементы"}
+
+
+    def test_func(self):
+        return can_access_object(self.request, "elements")
+
+
+class ElementsCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Element
+    form_class = ElementForm
+    success_url = reverse_lazy("elements:home")
+    template_name = "form.html"
+    extra_context = {"name": "Элемент"}
+
+    def test_func(self):
+        return can_access_object(self.request, "elements")
+
+
+class ElementsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Element
+    form_class = ElementForm
+    success_url = reverse_lazy("elements:home")
+    template_name = "form.html"
+    extra_context = {"name": "Элемент"}
+
+    def test_func(self):
+        return can_access_object(self.request, "elements")
+
+
+class ElementsDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Element
+    success_url = reverse_lazy("elements:home")
+    template_name = "confirm_delete.html"
+    extra_context = {"name": "Элемент"}
+
+    def test_func(self):
+        return can_access_object(self.request, "elements")
